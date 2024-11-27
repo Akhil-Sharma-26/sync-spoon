58d7aaf7e50a695dc8ca3fc4f7dc953d4781b4e9
Akhil-Sharma-26 <2004akhil26sharma@gmail.com>
2024-11-27 11:58:10 +0530

    Added-sentry

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,23 @@ import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://ff0241482299798c767bc54293404d9e@o4508368763551744.ingest.de.sentry.io/4508368767352912",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 const queryClient = new QueryClient();
 
@@ -13,5 +30,6 @@ createRoot(document.getElementById('root')!).render(
       <App />
       <ReactQueryDevtools/>
     </QueryClientProvider>
+    
   </StrictMode>,
 )
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,16 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "thapar-institute-of-enginee-c1",
+    project: "spoon-sync"
+  })],
   base: '/', // Explicitly set base path
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: true
   }
 })
